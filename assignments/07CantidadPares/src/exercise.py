28ebb4b29a4a39a6157fef173636c6ba1eed15dc
ALFAFL <89622984+ALFAFL@users.noreply.github.com>
2021-09-28 23:04:42 -0500

    Update exercise.py

--- assignments/07CantidadPares/src/exercise.py
+++ assignments/07CantidadPares/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass            
+p=int(0)
+
+n=int(input())
+
+while(n>=0):
+      if(n%2==0):
+          p=p+1
+      else:
+          p=p+0
+      n=int(input())    
+print("total de pares=",p)
 
 
 if __name__=='__main__':
