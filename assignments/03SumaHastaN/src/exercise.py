9086f0c73e01038b0eba951785859209c5fe4d81
ALFAFL <89622984+ALFAFL@users.noreply.github.com>
2021-09-29 10:53:07 -0500

    Update exercise.py

--- assignments/03SumaHastaN/src/exercise.py
+++ assignments/03SumaHastaN/src/exercise.py
@@ -1,6 +1,9 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-
-if __name__=='__main__':
-    main()
+def num(n):
+        list=[]
+        s=0
+        for i in range(0,n+1):
+            list.append(i)
+        for i in list:
+            s=s+i
+        return (s)
+    print(num(int(input())))
